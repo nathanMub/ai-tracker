1384b23e8ad14838a94461d8de925d7c8004df66
nathan mubvuta <135868806+nathanMub@users.noreply.github.com>
2026-06-27 06:54:28 +0300

    Refactor Tailwind configuration for content and plugins
    Removed dark mode configuration and updated content paths to include JSX and MDX file types. Cleared plugins array.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 }
